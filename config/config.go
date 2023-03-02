ac0d7871e7e6017edcccd9c8d98afebb454aef8d
blackriper <rodolfotovartorres@gmail.com>
2023-03-02 14:15:39 -0600

    :feature agregando config cors

--- config/config.go
+++ config/config.go
@@ -1,10 +1,19 @@
 package config
 
-import "github.com/joho/godotenv"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/joho/godotenv"
+)
 
 func LoadEnv() {
 	err := godotenv.Load()
 	if err != nil {
 		panic("Error loading .env file")
 	}
+}
+
+func CorsConfig() cors.Config {
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	return config
 }
--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"github.com/blackriper/inventory-api/config"
 	"github.com/blackriper/inventory-api/models"
 	"github.com/blackriper/inventory-api/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	config.LoadEnv()
+	r.Use(cors.New(config.CorsConfig()))
 	models.ConectionDatabase()
 	routes.Routes(r)
 	r.Run(":8000")
